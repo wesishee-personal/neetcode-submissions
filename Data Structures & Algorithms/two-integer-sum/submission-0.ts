43ac33ba5bbf826418be844466ec9097c5f749cb
Wes Ishee <wesishee@gmail.com>
2026-07-22 16:59:20 -0500

    Add: two-integer-sum - submission-0

---
+++ Data Structures & Algorithms/two-integer-sum/submission-0.ts
@@ -0,0 +1,18 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums: number[], target: number): number[] {
+        // slow af approach
+        for (let i = 0; i < nums.length-1; i++) {
+            for (let j = i+1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return [i,j];
+                }
+            }
+        }
+
+    }
+}
